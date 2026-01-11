78c7545ab807c3535e4297055fe15dd6a1089f53
Alexander Georgievskiy <galeksandrp@gmail.com>
2026-01-11 10:50:21 +0000

    flibusta.is

--- src/flibusta.py
+++ src/flibusta.py
@@ -21,7 +21,7 @@ from calibre.gui2.store.search_result import SearchResult
 class FlibustaStore(StorePlugin):
 
     open_search_url = 'https://raw.githubusercontent.com/alardus/flibusta-calibre-opds-store/main/opds-opensearch.xml'
-    web_url = 'https://flibusta.site/'
+    web_url = 'https://flibusta.is/'
 
     def open(self, parent=None, detail_item=None, external=False):
         if not hasattr(self, 'web_url'):
